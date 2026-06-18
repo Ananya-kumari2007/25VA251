0c019c3a53819b8c3ef120a9a6c220e1d1f460fb
Ananya kumari <raiananya1910@gmail.com>
2026-06-18 11:07:40 +0530

    assets

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import Exp7 from "./Exp7";
 import "./Exp7.css";
 import Exp8 from "./Exp8";
-
-
+import "./Exp8.css";
+import Exp9 from "./Exp9";
+import "./Exp9.css";
 
 
 function App() {
   return (
     <div>
-        < Exp7
+        < Exp8
           
         />
 
